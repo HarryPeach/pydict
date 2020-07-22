4580f60ace5e6e9618349c4db2ad70f1eb032a5f
Harry Peach (Personal Signing Key) <harrypeach1999@gmail.com>
2020-07-22 22:51:18 +0100

    added more tests for word class

--- pydict/word.py
+++ pydict/word.py
@@ -3,6 +3,11 @@ import json
 class Word:
     def __init__(self, json_input):
         json_parsed = json.loads(json_input)
-        # self.definiton = json_parsed["definitions"]
-        self.word = ""
-        self.definition = ""
+        try:
+            self.word = json_parsed["id"]
+            self.definition = json_parsed["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]
+            self.short_definition = json_parsed["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["shortDefinitions"][0]
+        except KeyError:
+            raise ValueError("The JSON passed to the class was malformed.")
+
+        
--- test/__main__.py
+++ test/__main__.py
@@ -16,13 +16,26 @@ class Test(unittest.TestCase):
 class TestWord(unittest.TestCase):
     """Tests the word class"""
 
-    def test_word_from_json(self):
-        """Tests the constructor of the Word class functions correctly"""
-        sample_json = importlib.resources.read_text(x, "sample_ace_response.json")
-        example_word = Word(sample_json)
+    def setUp(self):
+        self.sample_json = importlib.resources.read_text(x, "sample_ace_response.json")
+        self.example_word = Word(self.sample_json)
 
-        expect(example_word.word).to(equal("ace"))
-        # expect(example_word.definition).to(equal("a playing card with a single spot on it, ranked as the highest card in its suit in most card games"))
+    def test_word_from_json(self):
+        """Tests that the word class can successfully get a word from json response"""
+        expect(self.example_word.word).to(equal("ace"))
+
+    def test_definition_from_json(self):
+        """Tests that the word class can successfully get a definition from json response"""
+        expect(self.example_word.definition).to(equal("a playing card with a single spot on it, ranked as the highest card in its suit in most card games"))
+
+    def test_short_definition_from_json(self):
+        """Tests that the word class can successfully get a short definition from json response"""
+        expect(self.example_word.short_definition).to(equal("playing card with single spot on it, ranked as highest card in its suit in most card games"))
+
+    def test_invalid_json_indices(self):
+        """Test that the word initializer will safely fail when there is no matching indices"""
+        self.assertRaises(ValueError, Word, "{}")
+
 
 if __name__ == "__main__":
     unittest.main()
