6847bd151f94797a1d9c61c502f3fa32ff406c63
Harry Peach (Personal Signing Key) <harrypeach1999@gmail.com>
2020-07-24 13:51:18 +0100

    Added UNIX mode for piped input

--- pydict/core.py
+++ pydict/core.py
@@ -1,6 +1,7 @@
 import argparse
 import requests
 import os
+import sys
 
 from pydict.network_manager import NetworkManager
 
@@ -9,8 +10,12 @@ APP_ID_STRING = "PYDICT_APP_ID"
 API_URL = "https://od-api.oxforddictionaries.com:443/api/v2/entries/en-gb/{word}?fields=definitions"
 
 def main():
+    unix_mode = False
+    unix_args = ""
+
     parser = argparse.ArgumentParser(description="Simple dictionary lookup using Oxford Dictionaries API")
-    parser.add_argument("query", type=str, help="the search query")
+    parser.add_argument("-x", "--unix", type=bool, nargs="?", const=True, default=False, help="'Unix mode', enables piped input")
+    parser.add_argument("query", nargs="?", type=str, help="the search query")
     parser.add_argument("--app-id", type=str, help="the app ID to access the dictionaries API")
     parser.add_argument("--api-key", type=str, help="the API key to access the dictionaries API")
 
@@ -41,9 +46,20 @@ def main():
     else:
         APP_ID = os.getenv(APP_ID_STRING)
 
+    # Enable unix mode if specified
+    if ARGS.unix == True:
+        unix_mode = True
+        unix_args = sys.stdin.read().rstrip()
+    else:
+        if ARGS.query is None:
+            parser.error("A query is required if the program is not run in UNIX mode")
+
     json = ""
     try:
-        json = NETWORK_MANAGER.make_request(API_KEY, APP_ID, ARGS.query, "")
+        if unix_mode: 
+            json = NETWORK_MANAGER.make_request(API_KEY, APP_ID, unix_args, "")
+        else:
+            json = NETWORK_MANAGER.make_request(API_KEY, APP_ID, ARGS.query, "")
         words = NETWORK_MANAGER.words_from_json(json)
         print(words[0].definition)
     except Exception as e:
